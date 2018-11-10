9659110e932344cb09596f459cf97b0c7d616535
Glitch (hello-webpage) <none>
2018-11-10 04:38:09 +0000

    🗾⛪️ Checkpoint ./index.html:2187925/728 ./script.js:2187925/1543

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+$(document).ready(function(){
+  $("#sub").click(function(){
+    var name =  document.getElementById("name").value;
+    var email =  document.getElementById("email").value;
+    var msg =  document.getElementById("msg").value;
+    console.log(name, email, msg);
+    $.get("https://script.google.com/macros/s/AKfycby9S61GX0h8zrZaOVh3E-J2EMFq07qIP_Z3l2q2SbaKy9xgiWM/exec", { name:name, email:email, msg:msg });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("msg").value = "";
+    alert("Message sent successfully");
+  });
+});
